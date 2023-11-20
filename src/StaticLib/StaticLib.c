22d5d57f71f011c35f7f4488591ca86d45a4e19c
diceroll5656 <kuni0521iku@icloud.com>
2023-11-20 13:45:17 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -6,6 +6,12 @@
 
 #include "../include/lib_func.h"
 
+// ユーザー定義の構造体
+typedef struct {
+	int* addr;  // ダイナミック配列の先頭アドレス
+	int num;    // ダイナミック配列の要素数
+};
+
 // my_array を要素数nで初期化する
 void initialize(my_array* ar, int n)
 {
@@ -34,7 +40,30 @@ void release(my_array* ar)
 // my_array の要素数を変更する(以前の値は要素が同じ範囲で残す)
 void resize(my_array* ar, int n)
 {
-	// ToOo:配列の要素数を変更しよう！(reallocは禁止)
+	if (ar == NULL) return;  // NULL チェック
+
+	if (n < 0) n = 0; // 非正の値が来たら、とりあえず空にする
+
+	// 新しい領域を確保
+	int* newAddr = (int*)malloc(sizeof(int) * n);
+
+	if (newAddr == NULL) { // 確保できなかったら要素数は0とする
+		ar->num = 0;
+	}
+	else {
+		// 要素数が縮小される場合、元のデータをコピー
+		int minSize = (n < ar->num) ? n : ar->num;
+		for (int i = 0; i < minSize; ++i) {
+			newAddr[i] = ar->addr[i];
+		}
+
+		// 古い領域を解放
+		free(ar->addr);
+
+		// 新しい領域を設定
+		ar->addr = newAddr;
+		ar->num = n;
+	}
 }
 
 // my_array のindex番目の要素にvalを設定する
@@ -42,20 +71,30 @@ void resize(my_array* ar, int n)
 bool set(my_array* ar, int index, int val)
 {
 	// ToOo:配列の要素を変更しよう！
-	return false;
-}
+	if (ar == NULL || index < 0 || index >= ar->num) 
+		return false;
+
+	ar->addr[index] = val;
+	return true;
+	}
+	
 
 // my_array のindex番目の要素を取得する
 // index が確保されていない場所を指していたら0を返す
 int get(const my_array* ar, int index)
 {
 	// ToOo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+	if(ar == NULL || index < 0 || index >= ar->num) return 0; 
+
+	return ar->addr[index];
 }
+
 
 // my_array の要素数を取得する
 int size(const my_array* ar)
 {
 	// ToOo: 配列の要素数を返そう
-	return -1;
+	if (ar == NULL) return 0;
+
+	return ar->num;
 }
